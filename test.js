f1b3fb03b103138219d9699bdc382d88f050542a
JieJiSS <c141028@163.com>
2018-02-22 17:52:24 +0800

    update test.js

--- test.js
+++ test.js
@@ -7,14 +7,14 @@ const {
 } = require("./solver");
 
 (async () => {
-    let result = await main({
+    let result1 = await main({
         init: 0,
         goal: 10,
         move: 4,
         strarr: ["+5", "2", "reverse", "<shift"],
         door: ""
     });
-    assert.strictEqual(result.sort().toString(), ['Solution: +5, +5; process: 5->10',
+    assert.strictEqual(result1.sort().toString(), ['Solution: +5, +5; process: 5->10',
         'Solution: +5, reverse, +5; process: 5->5->10',
         'Solution: +5, <shift, +5; process: 5->5->10',
         'Solution: +5, reverse, reverse, +5; process: 5->5->5->10',
@@ -32,17 +32,25 @@ const {
         'Solution: <shift, reverse, +5, +5; process: 0->0->5->10',
         'Solution: <shift, <shift, +5, +5; process: 0->0->5->10'
     ].sort().toString(), "× failed");
-        main({
-            init: 0,
-            goal: 10,
-            move: 2,
-            strarr: ["WTF???"],
-            door: ""
-        }).then(() => {
-            assert.fail("× this should throw an Error");
-        }).catch(e => {
-            assert.ok(e instanceof TypeError, "× this should throw a TypeError");
-        });
+    let result2 = await main({
+        init: 0,
+        goal: -10,
+        move: 3,
+        strarr: ["3"],
+        door: ""
+    });
+    assert.strictEqual(result2.toString(), "No solution found.", "× expecting `No solution found.`");
+    main({
+        init: 0,
+        goal: 10,
+        move: 2,
+        strarr: ["WTF???"],
+        door: ""
+    }).then(() => {
+        assert.fail("× this should throw an Error");
+    }).catch(e => {
+        assert.ok(e instanceof TypeError, "× this should throw a TypeError");
+    });
     console.log("Test finished.");
 })().catch(e => {
     console.error(e);
